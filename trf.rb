2ca9d6ad2badd6e0b98575305be4506cca0dc311
Almix047 <Amix047@gmail.com>
2021-03-29 07:53:57 +0300

    Services

--- trf.rb
+++ trf.rb
@@ -16,55 +16,79 @@ end
 
 # Контроллеры
 class PromoMessagesController < ApplicationController
-
   def new
     @message = PromoMessage.new
-    if params[:date_from].present? && params[:date_to].present?
-      sample_of_users
+    @sample_of_users = SampleOfUsersService.call(params[:date_from], params[:date_to], params[:page])
     end
   end
 
   def create
     @message = PromoMessage.new(promo_message_params)
 
-    recipients = sample_of_users.pluck(:phone)
+    recipients = @sample_of_users.pluck(:phone)
 
-    if @message.save && send_message(recipients)
-      redirect_to promo_messages_path, notice: "Messages Sent Successfully!"
+    if @message.save
+      SendPromoMessageService.send_message(recipients)
+      redirect_to promo_messages_path, notice: 'Messages scheduled for sending.'
     else
-      render 'new', alert: "Something went wrong"
+      render 'new', alert: 'Something went wrong'
     end
   end
 
   def download_csv
-    users = sample_of_users
+    CsvReportService.new.call(params[:date_from], params[:date_to], params[:page])
+  end
+
+  private
+
+  def promo_message_params
+    params.permit(:body, :date_from, :date_to)
+  end
+end
+
+
+
+
+# Сервисы
+class SampleOfUsersService
+  def self.call(date_from, date_to, page)
+    if valid_date?(date_from) && valid_date?(date_to)
+      User.recent.joins(:ads).where('published_ads_count': 1)
+      .where('published_at': date_from..date_to)
+      .page(page)
+    end
+  end
+
+  private
+
+  def valid_date?(date)
+    date.present? && (Date.parse(date) rescue nil).is_a?(Date)
+  end
+end
+
+class CsvReportService
+  ATTRIBUTES = %w[id phone name]
+
+  def call(date_from, date_to, page)
+    users = SampleOfUsersService.call(date_from, date_to, page)
     send_data to_csv(users), filename: "promotion-users-#{Time.zone.today}.csv"
   end
 
   private
 
-    def to_csv(data)
-      attributes = %w(id phone name)
-      CSV.generate(headers: true) do |csv|
-        csv << attributes
-        data.each do |user|
-          csv << attributes.map { |attr| user.send(attr) }
-        end
+  def to_csv(data)
+    CSV.generate(headers: true) do |csv|
+      csv << ATTRIBUTES
+      data.each do |user|
+        csv << ATTRIBUTES.map { |attr| user.send(attr) }
       end
     end
+  end
 
-    def send_message(recipients)
-      recipients.each do |r|
-        PromoMessagesSendJob.perform_later(r)
-      end
-    end
+end
 
-    def sample_of_users
-      @users = User.recent.joins(:ads).group("ads.user_id").where("`published_ads_count` = 1").
-        where("published_at Between ? AND ?", Date.parse(params[:date_from]), Date.parse(params[:date_to])).page(params[:page])
-    end
-
-    def promo_message_params
-      params.permit(:body, :date_from, :date_to)
-    end
+class SendPromoMessageService
+  def send_message(recipients)
+    recipients.each { |r| PromoMessagesSendJob.perform_later(r) }
+  end
 end
